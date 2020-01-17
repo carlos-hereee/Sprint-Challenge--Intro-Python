8e877b5285850ecf3b542811faf4d2937fea41fc
ch04937 <97hernandez.c@gmail.com>
2020-01-17 10:00:40 -0800

    oop2 complete

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -3,12 +3,22 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
+
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, name,  num_wheels=4):
+        # change it so the num_wheels defaults to 4 if not specified
+        # self.num_wheels = num_wheels
         self.num_wheels = num_wheels
+        self.name = name
 
-    # TODO
+    # Todo add method drive() that returns "vroooom".
+    def drive(self):
+        return f'vroooom'
 
+
+g = GroundVehicle('ground unit')
+print('\nnumber of wheels is 4', g.num_wheels)
+print('\nground unit goes', g.drive())
 
 # Subclass Motorcycle from GroundVehicle.
 #
@@ -17,16 +27,35 @@ class GroundVehicle():
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
-# TODO
+
+# Todo # Subclass Motorcycle from GroundVehicle.
+class Motorcycle(GroundVehicle):
+    # #of wheels to 2
+    def __init__(self, name,  num_wheels=2):
+        super().__init__(num_wheels)
+        self.name = name
+
+    # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+
+    def drive(self):
+        return f'BRAAAP'
+
+
+m = Motorcycle('motorcycle')
+print('\nmotercycle # of wheels is 2', m.num_wheels)
+print('\nmotercycle goes', m.drive())
 
 vehicles = [
-    GroundVehicle(),
-    GroundVehicle(),
-    Motorcycle(),
-    GroundVehicle(),
-    Motorcycle(),
+    GroundVehicle('ground unit'),
+    GroundVehicle('ground unit'),
+    Motorcycle('motorcycle'),
+    GroundVehicle('ground unit'),
+    Motorcycle('motorcycle'),
 ]
 
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+
+for vehicle in vehicles:
+    print(f'\n and this {vehicle.name} goes', vehicle.drive())
